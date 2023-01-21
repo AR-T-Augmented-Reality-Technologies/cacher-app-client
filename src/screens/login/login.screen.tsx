f86132a5b7c2e1e468595081d7f926257df9f24d
Jacob Mowat <zaxquit@protonmail.com>
2023-01-21 21:16:53 +0000

    Added html for Login Screen and styles
    - Added html code from App Assets to the screen.
    - Added some padding and margin around buttons.

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, View, ImageBackground } from "react-native";
+
+import "./login.styles.css";
 
 interface LoginScreenProps {
     navigation: any;
 }
 
 export const LoginScreen = ({ navigation }: LoginScreenProps) => {
-    return <View>
-        LoginScreen
-        <Button title="Press me" onPress={() => {
-            navigation.navigate('Register');
-        }}></Button>
-    </View>
+
+    return <>
+        <div className="vline1"></div>
+        <div className="vline2"></div>
+        <div className="vline3"></div>
+        <img className="logo" src={"images/cacherr.png"} alt="cacher logo" style={{"width": "10%"}} />
+        <div className="login">
+            <form>
+                <div className="forminner">
+                    <label htmlFor="email"> Email: </label><br />
+                    <input type="text" id="email" name="Email" /><br />
+                    <label htmlFor="password"> Password: </label><br />
+                    <input type="password" id="password" name="Password" /><br />	
+                </div>
+                <div style={{"margin": "10px"}}>
+                    <button onClick={() => {navigation.navigate('Register')}}>Register</button>
+                    <button>Login</button>
+                </div>
+            </form>
+        </div>
+    </>
 };
