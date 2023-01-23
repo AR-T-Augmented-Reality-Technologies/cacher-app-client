8272bc6bc08f5383a43cfa085d2dc6e7ace26364
Kacper Mazur <kacper@M2Air.local>
2023-01-23 22:51:51 +0000

    syntax fixes

--- src/screens/image/image.screen.tsx
+++ src/screens/image/image.screen.tsx
@@ -21,19 +21,19 @@ export const  ImageScreen = ({ navigation }: ImageScreenProps) => {
         return true;
     };
 
-    return<>
+    return <>
         <div className = "homebut">
             <button onClick={() => {navigation.navigate('MapScreen')}}>Comment</button>
         </div>
-        <div className = "userengage">\
+        <div className = "userengage">
             <button onClick={() => {navigation.navigate('LikedImage')}}>Like</button>
             <button onClick={() => {navigation.navigate('ShareImage')}}>Share</button>
             <button onClick={() => {navigation.navigate('Commented')}}>Comment</button>
         </div>
         <div className = "options">
-            <img id="options" src="images/options.png" onclick="openImg()" onClick={() => {navigation.navigate('MapScreenwithOptions')}}>  
+            <img id="options" src="images/options.png" onClick={() => {navigation.navigate('MapScreenwithOptions')}}/>  
         </div>
-        <label for="Caption">Caption:</label>
-        <input type="text" id="Caption" name="Caption" placeholder="User Caption"
+        <label htmlFor="Caption">Caption:</label>
+        <input type="text" id="Caption" name="Caption" placeholder="User Caption"> </input>
     </>
     };
