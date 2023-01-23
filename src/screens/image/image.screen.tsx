b2354177e0d03053367c24b10d0baf14049299cb
AlbaraaOthman <ao2000@hw.ac.uk>
2023-01-23 21:20:41 +0000

    Added scrapbok page scren

---
+++ src/screens/image/image.screen.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button, View, ImageBackground } from "react-native";
+
+
+interface ImageScreenProps {
+    navigation: any;
+}
+
+export const  ImageScreen = ({ navigation }: ImageScreenProps) => {
+    const [location, setLocation] = useState("");
+    const validateForm = (e: any) => {
+        e.preventDefault();
+
+        // TODO: Form validation here
+        
+        return false;
+    };
+
+    const submitForm = () => {
+        // TODO: Form submit to server using REST API.
+        return true;
+    };
+
+    return<>
+        <div className = "homebut">
+            <button onClick={() => {navigation.navigate('MapScreen')}}>Comment</button>
+        </div>
+        <div className = "userengage">\
+            <button onClick={() => {navigation.navigate('LikedImage')}}>Like</button>
+            <button onClick={() => {navigation.navigate('ShareImage')}}>Share</button>
+            <button onClick={() => {navigation.navigate('Commented')}}>Comment</button>
+        </div>
+        <div className = "options">
+            <img id="options" src="images/options.png" onclick="openImg()" onClick={() => {navigation.navigate('MapScreenwithOptions')}}>  
+        </div>
+        <label for="Caption">Caption:</label>
+        <input type="text" id="Caption" name="Caption" placeholder="User Caption"
+    </>
+    };
