7a23e60f04ab5ecf0e77515682af6e61666b4a47
Jacob Mowat <zaxquit@protonmail.com>
2023-02-01 03:31:29 +0000

    Register screen connected to API

--- src/screens/register/register.screen.tsx
+++ src/screens/register/register.screen.tsx
@@ -19,11 +19,34 @@ export const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
     const validateForm = (e: any) => {
         e.preventDefault(); 
         // TODO: form validation
-        return false;
+        return true;
     };
 
     const submitForm = () => {
-        return true;
+        const data = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "password": password,
+            "dob": dob,
+            "username": username
+        };
+
+        fetch('http://localhost:4000/api/users/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            // Set our user
+            return data;
+        }
+        );
+
+        navigation.navigate('Login')
     };
 
     return <>
@@ -47,47 +70,47 @@ export const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
                     <div className="mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="emailLabel">Email</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="email-input" type="email"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="email-input" type="email" onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="usernameLabel">Username</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="username-input" type="text"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="username-input" type="text" onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="passwordLabel">Password</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="password-input" type="password"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="password-input" type="password" onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="passwordLabel">Confirm Password</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="confirmPassword-input" type="password"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="confirmPassword-input" type="password" onChange={(e) => setPasswordConfirm(e.target.value)}/>
                     </div>
                     <div className="flex items-center justify-between">
                     <div className="mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="fnameLabel">First Name</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="fname-input" type="text"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="fname-input" type="text" onChange={(e) => setFirstname(e.target.value)}/>
                     </div>
                     <div className="ml-6 mb-4">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="lnameLabel">Last Name</label>
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="lname-input" type="text"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="lname-input" type="text" onChange={(e) => setLastname(e.target.value)}/>
                     </div>
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-500 font-bold mb-2" htmlFor="dobLabel">Date of Birth</label>
                         <input className="bg-gray-200 appearance-none border-2 h-10 border-gray-200 rounded w-full py-2 px-4
-                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="dob-input" type="date"/>
+                         text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-custom-blue" id="dob-input" type="date" onChange={(e) => setDob(e.target.value)}/>
                     </div>
                     <div className="flex mb-8">
-                        <input className="form-checkbox accent-custom-blue" type="checkbox" id="registerCheckbox" />
+                        <input className="form-checkbox accent-custom-blue" type="checkbox" id="registerCheckbox" onChange={(e) => setRegisterCheckbox(e.target.checked)}/>
                         <label className="ml-2" htmlFor="registerCheckbox">I agree to my details being registered</label>
                     </div>
                     <div className="flex justify-center mb-8">
                         <button className="bg-custom-blue hover:bg-custom-blue-hover text-white font-bold py-2 px-4 rounded 
-                        focus:outline-none focus:shadow-outline" type="button" onClick={() => {navigation.navigate('Login')}}>Register</button>
+                        focus:outline-none focus:shadow-outline" type="button" onClick={(e) => {validateForm(e) && submitForm()}}>Register</button>
                     </div>
                 </form>
             </div>
