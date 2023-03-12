9e5bff61698563ca1691305a837248da7e3685a0
AlbaraaOthman <ao2000@hw.ac.uk>
2023-03-12 13:42:31 +0000

    Completed generate sumamry button
    fixed showing tickets too

--- src/screens/admin-dashboard/ticket-report.screen.tsx
+++ src/screens/admin-dashboard/ticket-report.screen.tsx
@@ -17,6 +17,7 @@ interface TicketScreenProps {
 export const TicketScreen = ({ navigation }: TicketScreenProps) => {
     const user = useSelector((state: any) => state.users);
     const [currentPage, setCurrentPage] = useState("TicketScreen");
+    const [showTick, setTick] = useState(false);
     const [idarr, setIdArr] = useState<number[]>([]);
     const [reporterarr, setReporterarr] = useState<number[]>([]);
     const [pagearr, setPagearr] = useState<number[]>([]);
@@ -28,7 +29,9 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
     let reporterArr = [0];
     let pageidArr = [0];
     let reasonArr = ["balls"];
-    let resolved = [false];
+    let resolved = [true];
+
+  
 
     useEffect(() => {
         const storedPage = localStorage.getItem("currentPage");
@@ -41,9 +44,19 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
         localStorage.setItem("currentPage", currentPage);
     }, [currentPage]);
 
+    // const empty = () => {
+    //     setIdArr([]);
+    //     setReporterarr([]);
+    //     setPagearr([]);
+    //     setReasonarr([]);
+    //     setresolvedarr([]);
+    // }
+  
+    const displayTick = () => {
+        setTick(!showTick);
+    };
 
     const getReports = async () => {
-        console.log('back her')
         const response = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getReports`, {
             method: "POST",
             headers: {
@@ -51,23 +64,18 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
             },
         });
         const data = await response.json();
-        console.log(data);
-
         reportidArr[0] = (data.data.reported_posts[0].reportid);
         reporterArr[0] = data.data.reported_posts[0].reporter_id;
         pageidArr[0] = (data.data.reported_posts[0].page_id);
         reasonArr[0] = data.data.reported_posts[0].reason;
         resolved[0] = data.data.reported_posts[0].issue_resolved;
-        console.log('LENGHT LOP  ' + data.data.reported_posts.length);
         for (var i = 1; i < data.data.reported_posts.length; i++) {
-            console.log('here')
             reportidArr[i] = (data.data.reported_posts[i].reportid);
             reporterArr[i] = (data.data.reported_posts[i].reporter_id);
             pageidArr[i] = (data.data.reported_posts[i].page_id);
             reasonArr[i] = (data.data.reported_posts[i].reason);
             resolved[i] = (data.data.reported_posts[i].resolved);
         }
-        console.log('Length of reports is ' + reportidArr.length);
 
         for (var j = 0; j < reportidArr.length; j++) {
             idarr.push(reportidArr[j]);
@@ -76,11 +84,8 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
             reasonarr.push(reasonArr[j]);
             resolvedarr.push(resolved[j]);
         }
-
-
     }
-
-    const handleResolver = async (id : any) => {
+    const handleResolver = async (id: any) => {
         const payload = {
             repid: id,
         }
@@ -94,10 +99,67 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
         });
         getReports();
     }
+
+    const handleSummary = async () => {
+        const responseUser = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getUsers`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const dataUser = await responseUser.json();
+
+        const responseCom = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getCom`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const dataCom = await responseCom.json();
+
+        const responseImage = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getImage`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const dataImage = await responseImage.json();
+
+        const responseResp = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getResponded`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const dataResp = await responseResp.json();
+
+        const responseScrap = await fetch(`${process.env.REACT_APP_REST_API_HOST}/admin/getScrap`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const dataScrap = await responseScrap.json();
+
+        const summary = "Currently cacher has registered " + dataUser.data.usercount + " users, has " + dataCom.data.comcount + " stored comments, on " + dataImage.data.imcount + " images and " + dataScrap.data.scrapcount + " scrapbooks. The admins have been hard at work and responded to " + dataResp.data.respcount + " reports"
+
+        const downloadTxtFile = () => {
+            const element = document.createElement("a");
+            const file = new Blob([summary], {
+                type: "text/plain"
+            });
+            element.href = URL.createObjectURL(file);
+            element.download = "summaryreport.txt";
+            document.body.appendChild(element);
+            element.click();
+        }
+        downloadTxtFile()
+    }
     if (gotCommFlag == 0) {
         getReports();
         setGotCommFlag(1);
     }
+
     return (
         <>
             <div className="grid grid-cols-8 min-h-screen dark:bg-dback">
@@ -136,46 +198,66 @@ export const TicketScreen = ({ navigation }: TicketScreenProps) => {
                         className="w-1/2 md:w-1/6 mx-auto mt-4"
                     ></img>
                     <div className="grid grid-cols-1 pl-5 pr-5 overflow-auto dark:bg-dback">
-                        {idarr.map((report, index) => (
-                            <div
-                                key={index}
-                                className="dark:bg-dback bg-white border-solid border border-black break-normal h-auto rounded-lg mb-5 grid grid-cols-5 grid-rows-2"
-                            >
-                                <p
-                                    className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-1 text-sm overflow-hidden text-justify"
-                                    style={{ wordWrap: "break-word" }}
-                                >
-                                    {"Report ID - " + report}{" "}
-                                </p>
-                                <button className="pb-2 px-5 text-s row-start-3 bg-custom-blue dark:bg-dblue hover:bg-custom-blue-hover hover:dark:bg-dorange text-white font-bold py-2 px-4 rounded 
+                        {showTick ? (
+                            <>
+                                <button className="bg-custom-blue dark:bg-dblue hover:bg-custom-blue-hover hover:dark:bg-dorange text-white font-bold py-2 px-4 rounded 
+                focus:outline-none focus:shadow-outline" onClick={displayTick}> Hide Tickets</button>
+                            </>
+                        ) : (
+                            <>
+                                <button className="bg-custom-blue dark:bg-dblue hover:bg-custom-blue-hover hover:dark:bg-dorange text-white font-bold py-2 px-4 rounded 
+                focus:outline-none focus:shadow-outline" onClick={displayTick}> Show Tickets</button>
+                            </>
+                        )}
+                        {showTick && (
+                            <>
+                                {
+                                    idarr.map((report, index) => (
+                                        <div
+                                            key={index}
+                                            className="dark:bg-dback bg-white border-solid border border-black break-normal h-auto rounded-lg mb-5 grid grid-cols-5 grid-rows-2"
+                                        >
+                                            <p
+                                                className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-1 text-sm overflow-hidden text-justify"
+                                                style={{ wordWrap: "break-word" }}
+                                            >
+                                                {"Report ID - " + report}{" "}
+                                            </p>
+                                            <button className="pb-2 px-5 text-s row-start-3 bg-custom-blue dark:bg-dblue hover:bg-custom-blue-hover hover:dark:bg-dorange text-white font-bold py-2 px-4 rounded 
                 focus:outline-none focus:shadow-outline " onClick={() => handleResolver(report)}> Resolved</button>
-                                <p
-                                    className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-2 text-sm overflow-hidden text-justify"
-                                    style={{ wordWrap: "break-word" }}
-                                >
-                                    {"Page ID - " + (pagearr[index])}
+                                            <p
+                                                className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-2 text-sm overflow-hidden text-justify"
+                                                style={{ wordWrap: "break-word" }}
+                                            >
+                                                {"Page ID - " + (pagearr[index])}
 
-                                </p>
-                                <p className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-3 text-sm overflow-hidden text-justify">
-                                    Reporter User ID - {reporterArr[index]}
-                                </p>
-                                <p
-                                    className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-4 text-sm overflow-hidden text-justify"
-                                    style={{ wordWrap: "break-word" }}
-                                >
-                                    {"Report Reason - " + (reasonarr[index])}
+                                            </p>
+                                            <p className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-3 text-sm overflow-hidden text-justify">
+                                                Reporter User ID - {reporterArr[index]}
+                                            </p>
+                                            <p
+                                                className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-4 text-sm overflow-hidden text-justify"
+                                                style={{ wordWrap: "break-word" }}
+                                            >
+                                                {"Report Reason - " + (reasonarr[index])}
 
-                                </p>
-                                <p
-                                    className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-5 text-sm overflow-hidden text-justify"
-                                    style={{ wordWrap: "break-word" }}
-                                >
-                                    {"Resolved? - " + (resolvedarr[index])}
+                                            </p>
+                                            <p
+                                                className="break-normal px-2 py-2 col-start-1 col-span-3 row-start-5 text-sm overflow-hidden text-justify"
+                                                style={{ wordWrap: "break-word" }}
+                                            >
+                                                {"Resolved? - " + (resolvedarr[index])}
 
-                                </p>
-                            </div>
-                        ))}
+                                            </p>
+                                        </div>
+                                    ))
+                                }
+                            </>
+                        )}
                     </div>
+                    <button className="dark:text-white dark:bg-dback w-16 h-16 rounded-full text-xs text-black bg-white font-bold border-solid border-2" onClick={() => handleSummary()}>
+                        Generate Summary
+                    </button>
                 </div>
 
                 {/* A column on the right of the screen to display vertial lines. All 3 lines will be displayed on desktop. On mobile devices only 2 lines will be displayed to save space*/}
